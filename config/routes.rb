da42432c06716c5d5802a7a110b07d3523d8fb6a
briandangerflynn <bdflynny@gmail.com>
2018-04-12 22:02:00 -0400

    add root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'customers#new_order'
+
   resources :sessions, only: [:create, :new, :destoy]
     get '/logout', to: 'sessions#destroy'
     get '/login', to: 'sessions#new'
