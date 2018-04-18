41dd9568657b61523e1f3ab2bed9327b6f3edb35
briandangerflynn <bdflynny@gmail.com>
2018-04-18 15:48:02 -0400

    add jpgs and gifs to pop ups

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,7 @@
   var currentAlteration;
   var currentAltName = [];
   var currentAltPrice = [];
+  var currentPrep = {name: "", instructions: "", gif: "", jpg: ""}
   var currentItem;
   var itemPrice = 0;
   var items = [];
@@ -95,19 +96,25 @@
     }
   });
 
-  $(document).on("click", ".how-to-prep-button", function(){
-    if ($("#how-to-prep-popup").hasClass('hidden')){
-      var currentPrep = $(this).prev().find("p").html()
-      console.log(currentPrep)
-    $("#overlay, #how-to-prep-popup").fadeToggle();
+
+
+  $(document).on("click", ".prep-button", function(){
+    if ($("#prep-popup").hasClass('hidden')){
+      currentPrep.name = $(this).prev().find("p").html()
+      popup();
+      $("#prep-alt-name").html(currentPrep.name);
+      $("#prep-instructions").html(currentPrep.instructions);
+      $("#prep-jpg").attr('src', currentPrep.jpg);
+      $("#prep-gif").attr('src', currentPrep.gif);
+    $("#overlay, #prep-popup").fadeToggle();
 
     } else {
 
     }
   })
 
-  $(document).on("click", "#exit-prep", function(){
-    $("#overlay, #how-to-prep-popup").fadeToggle();
+  $(document).on("click", "#prep-exit", function(){
+    $("#overlay, #prep-popup").fadeToggle();
   });
 
 
@@ -301,11 +308,6 @@
       $("#review-total-price").html("$" + (totalPrice + 6).toFixed(2))
       $("#form-amount").val(totalPrice + 6)
 
-
-
-      // $($(".stripe-button")[0]).attr("data-amount", totalPrice)
-      // console.log($($(".stripe-button")[0]).attr("data-amount"))
-
     });
   // end of checkout button
 
@@ -369,7 +371,6 @@
 
     // hide review page and basket, return to garments page
     if (items.length === 0){
-      console.log("fuck")
       $("main").toggleClass('hidden');
       $("#review-order").toggleClass('hidden');
       // $("#basket").toggleClass('hidden');
@@ -462,6 +463,195 @@
   // AIR TAILOR API KEY: O7iq7W0Kcg8MynMp3aaHzgtt
 
   // QwZL2CvAUf8V1vYHZIc2Zgtt
+
+
+  //  HOW TO PIN POP-UPS
+
+  var gif1 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/1.gif"
+  var gif2 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/2.gif"
+  var gif3 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/3.gif"
+  var gif4 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/4.gif"
+  var gif5 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/5.gif"
+  var gif6 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/6.gif"
+  var gif7 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/7.gif"
+  var gif8 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/8.gif"
+  var gif9 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/9.gif"
+  var gif10 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/10.gif"
+  var gif11 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/11.gif"
+  var gif12 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/12.gif"
+  var gif13 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/13.gif"
+  var gif14 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/14.gif"
+  var gif15 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/15.gif"
+  var gif16 = "https://s3.us-east-2.amazonaws.com/airtailor-images/pin-gifs/16.gif"
+
+  var jpg1 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/1.jpg"
+  var jpg3 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/3.jpg"
+  var jpg4 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/4.jpg"
+  var jpg5 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/5.jpg"
+  var jpg6 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/6.jpg"
+  var jpg7 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/7.jpg"
+  var jpg8 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/8.jpg"
+  var jpg9 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/9.jpg"
+  var jpg10 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/10.jpg"
+  var jpg11 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/11.jpg"
+  var jpg12 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/12.jpg"
+  var jpg13 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/13.jpg"
+  var jpg14 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/14.jpg"
+  var jpg15 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/15.jpg"
+  var jpg19 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/19.jpg"
+  var jpg20 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/20.jpg"
+  var jpg21 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/21.jpg"
+  var jpg22 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/22.jpg"
+  var jpg26 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/26.jpg"
+  var jpg27 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/27.jpg"
+  var jpg29 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/28.jpg"
+  var jpg30 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/30.jpg"
+  var jpg31 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/31.jpg"
+  var jpg32 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/32.jpg"
+  var jpg33 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/33.jpg"
+  var jpg35 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/35.jpg"
+  var jpg36 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/36.jpg"
+  var jpg37 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/37.jpg"
+  var jpg38 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/38.jpg"
+  var jpg39 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/39.jpg"
+  var jpg40 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/40.jpg"
+  var jpg41 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/41.jpg"
+  var jpg42 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/42.jpg"
+  var jpg43 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/43.jpg"
+  var jpg44 = "https://s3.us-east-2.amazonaws.com/airtailor-images/new_how_to_pin/44.jpg"
+
+  // Shorten Dress
+  var instructions1 = "Place safety pin at desired dress length. Use multiple pins to outline a new shape, or one pin to maintain the current shape. If multiple layers, just mark outer layer."
+
+  // Shorten Sleeve
+  var instructions2 = "Place safety pin at desired sleeve length. Pin both sleeves for different lengths."
+
+  // Shorten Shoulder Straps
+  var instructions3 = "Pinch excess fabric on shoulder straps to desired fit and safety pin the fabric together. Make sure bust and waist still fit comfortably."
+
+  // Shorten Necktie
+  var instructions4 = "At the desired shortening location, place a safety on the back of the skinny end of the tie."
+
+  // Slim Necktie
+  var instructions5 = "Good news! No prep involved on this service. Just make sure that this is the width that you would like the widest part of your tie slimmed to."
+
+  // Cuffed Hem
+  var instructions6 = "Place safety pin at desired pant length. Pin both legs for different lengths. Even if you need to pin on the cuff to mark the desired length, the look and size of the original cuff will remain the same."
+
+  // Original Hem
+  var instructions7 = "Place safety pin at desired pant length. Pin both legs for different lengths. An Original Hem is different from a Regular Hem because it will maintain the exact hem at the bottom of the pant as it was originally produced after the pants are shortened. This can be used for frayed or raw edges, or to retain the original stitching from the manufacturer. This type of hem is most commonly used for premium denim."
+
+  // Regular Hem
+  var instructions8 = "Place safety pin at desired pant length. Pin both legs for different lengths."
+
+  // Taper Pants
+  var instructions9 = "On the inside of one pant leg, safety pin the fabric together in a line along the leg to desired fit. Leave enough room to put on/take off the pants."
+
+  // Shorten Shirt Hem
+  var instructions10 = "Place safety pin at desired shirt length. Use multiple pins to outline a new shape, or one pin to maintain the current shape."
+
+  // Take In Shoulders
+  var instructions11 = "Place a safety pin at desired shoulder seam area. Be mindful of final sleeve length if also shortening sleeves."
+
+  // Shorten Skirt Single Layer
+  var instructions12 = "Place safety pin at desired skirt length. Use multiple pins to outline a new shape, or one pin to maintain the current shape."
+
+  // Shorten Skirt Multiple Layer
+  var instructions13 = "Place safety pin at desired skirt length. Use multiple pins to outline a new shape, or one pin to maintain the current shape. If multiple layers, just mark outer layer."
+
+  // Shorten Jacket Length
+  var instructions14 = "Place safety pin at desired jacket length. Use multiple pins to outline a new shape, or one pin to maintain the current shape."
+
+  var popup = function() {
+    console.log(currentGarment)
+    if (currentGarment === "pants"){
+      console.log("well fuck")
+      switch (currentPrep.name) {
+        case 'Shorten Pant Length - Regular Hem':currentPrep = {name:currentPrep.name, instructions:instructions8, gif:gif8, jpg:jpg1};
+        break;
+        case 'Shorten Pant Length - Original Hem':currentPrep = {name:currentPrep.name, instructions:instructions7, gif:gif2, jpg:jpg1};
+        break;
+        case 'Shorten Pant Length - Cuffed Hem':currentPrep = {name:currentPrep.name, instructions:instructions6, gif:gif1, jpg:jpg3};
+        break;
+        case 'Slim Pants Legs (Taper) - Full Leg':currentPrep = {name:currentPrep.name, instructions:instructions9, gif:gif9, jpg:jpg44};
+        break;
+        case 'Slim Pants Legs (Taper) - Half Leg':currentPrep = {name:currentPrep.name, instructions:instructions9, gif:gif10, jpg:jpg4};
+        break;
+      }
+    } else if (currentGarment === "shirt"){
+
+      switch (currentPrep.name) {
+        case 'Shorten Shirt Sleeves — With Cuff':currentPrep = {name:currentPrep.name, instructions:instructions2, gif:gif13, jpg:jpg7};
+        break;
+        case 'Shorten Shirt Sleeves — Without Cuff':currentPrep = {name:currentPrep.name, instructions:instructions2, gif:gif11, jpg:jpg8};
+        break;
+        case 'Shorten Shirt Straps':currentPrep = {name:currentPrep.name, instructions:instructions3, gif:gif16, jpg:jpg43};
+        break;
+        case 'Shorten Dress Shirt (Hem)':currentPrep = {name:currentPrep.name, instructions:instructions10, gif:gif14, jpg:jpg11};
+        break;
+        case 'Shorten T-Shirt (Hem)':currentPrep = {name:currentPrep.name, instructions:instructions10, gif:gif12, jpg:jpg12};
+        break;
+        case 'Take In Shirt Shoulders':currentPrep = {name:currentPrep.name, instructions:instructions11, gif:gif15, jpg:jpg13};
+        break;
+      }
+    } else if (currentGarment === "skirt"){
+
+       switch (currentPrep.name) {
+        case 'Shorten Skirt (Hem) — Single Layer':currentPrep = {name:currentPrep.name, instructions:instructions12, gif:gif3, jpg:jpg14};
+        break;
+        case 'Shorten Skirt (Hem) — 2 Layers':currentPrep = {name:currentPrep.name, instructions:instructions13, gif:gif3, jpg:jpg14};
+        break;
+        case 'Shorten Skirt (Hem) — 3 Layers':currentPrep = {name:currentPrep.name, instructions:instructions13, gif:gif3, jpg:jpg14};
+        break;
+      }
+    } else if (currentGarment === "suit jacket"){
+
+       switch (currentPrep.name) {
+        case 'Shorten Jacket Length':currentPrep = {name:currentPrep.name, instructions:instructions14, gif:gif4, jpg:jpg26};
+        break;
+        case 'Shorten Jacket Sleeves':currentPrep = {name:currentPrep.name, instructions:instructions2, gif:gif5, jpg:jpg27};
+        break;
+        case 'Take In Jacket Shoulders':currentPrep = {name:currentPrep.name, instructions:instructions11, gif:gif6, jpg:jpg31};
+        break;
+      }
+    } else if (currentGarment === "dress"){
+
+      switch (currentPrep.name) {
+        case 'Shorten Dress Sleeves — With Cuff':currentPrep = {name:currentPrep.name, instructions:instructions2, gif:gif11, jpg:jpg42};
+        break;
+        case 'Shorten Dress Sleeves — Without Cuff':currentPrep = {name:currentPrep.name, instructions:instructions2, gif:gif13, jpg:jpg42};
+        break;
+        case 'Shorten Dress Straps':currentPrep = {name:currentPrep.name, instructions:instructions4, gif:gif16, jpg:jpg43};
+        break;
+        case 'Shorten Dress (Hem) — Single Layer':currentPrep = {name:currentPrep.name, instructions:instructions1, gif:gif3, jpg:jpg19};
+        break;
+        case 'Shorten Dress (Hem) — 2 Layers':currentPrep = {name:currentPrep.name, instructions:instructions1, gif:gif3, jpg:jpg19};
+        break;
+        case 'Shorten Dress (Hem) — 3 Layers':currentPrep = {name:currentPrep.name, instructions:instructions1, gif:gif3, jpg:jpg19};
+        break;
+      }
+    } else if (currentGarment === "necktie"){
+
+      switch (currentPrep.name) {
+        case 'Shorten Necktie':currentPrep = {name:currentPrep.name, instructions:instructions4, gif:gif7, jpg:jpg39};
+        break;
+        case 'Slim Necktie — 2 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 2.25 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 2.5 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 2.75 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 3 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 3.25 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+        case 'Slim Necktie — 3.5 Inches':currentPrep = {name:currentPrep.name, instructions:instructions5, gif:"", jpg:""};
+        break;
+      }
+    }
+  }
 
 
 
