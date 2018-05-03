9f49d940918ca80387bcffebde4ba9617286e8ad
briandangerflynn <bdflynny@gmail.com>
2018-05-03 08:16:24 -0400

    turn stripe back to live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,8 +5,8 @@ if Rails.env.development?
   }
 elsif Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_SAfpkFB2WVKLbQVukGCTVq5Z',
-    :secret_key      => 'sk_test_BRPpISwsTFnO5ih1WlxbvSkz'
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
   }
 end
 
