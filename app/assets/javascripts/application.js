735c5abaed9cb535f2ee54893298d446f2c9a32d
briandangerflynn <bdflynny@gmail.com>
2018-05-07 15:05:57 -0400

    remove confirmation alert about order deleting when you log out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -497,7 +497,6 @@
 
     // delete current div, subtract price from total, and remove current item from items array
     currentDiv.remove()
-    console.log(currentItem)
     totalPrice = totalPrice - currentItem.total
     $("#total-price").html(totalPrice.toFixed(2))
     items.splice(currentIndex,1)
