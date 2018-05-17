73c3a15df59d579bcdb0122763595e18f230ef28
briandangerflynn <bdflynny@gmail.com>
2018-05-17 19:01:28 -0400

    update alert email

--- app/mailers/airtailor_mailer.rb
+++ app/mailers/airtailor_mailer.rb
@@ -4,14 +4,14 @@ class AirtailorMailer < ApplicationMailer
     @customer = customer
     @data = data
     @res = res
-    mail(to: "team@airtailor.com", subject: "D2C ERROR — Customer: " + @customer.first_name + " " + @customer.last_name)
+    mail(to: "brian@airtailor.com", subject: "D2C ERROR — Customer: " + @customer.first_name + " " + @customer.last_name)
   end
 
   def success_email(customer, data, res)
     @customer = customer
     @data = data
     @res = res
-    mail(to: "team@airtailor.com", subject: "New D2C Order — Customer: " + @customer.first_name + " " + @customer.last_name)
+    mail(to: "brian@airtailor.com", subject: "New D2C Order — Customer: " + @customer.first_name + " " + @customer.last_name)
   end
 
 end
