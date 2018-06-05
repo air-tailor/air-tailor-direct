f79977dc0e94b429bddd4ddf1295940f86090421
briandangerflynn <bdflynny@gmail.com>
2018-06-05 11:24:17 -0400

    remove phone validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,6 @@ class Customer < ApplicationRecord
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-  validates :phone, presence: true
 
   validates :password, presence: true, length: { minimum: 5 }, :allow_blank => true
 
