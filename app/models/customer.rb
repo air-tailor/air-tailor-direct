9769da3df3f3edb5ab243efb820ea85d838727be
briandangerflynn <bdflynny@gmail.com>
2018-06-05 10:40:28 -0400

    remove SF tailor, add jquery to manage phone number format

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,8 @@ class Customer < ApplicationRecord
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
+  validates :phone, presence: true
+
   validates :password, presence: true, length: { minimum: 5 }, :allow_blank => true
 
   before_save { |customer| customer.email = email.downcase }
