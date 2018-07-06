d4eb5212c53a39eba0dae034dfaf715e738f0a87
briandangerflynn <bdflynny@gmail.com>
2018-07-06 12:22:26 -0400

    remove accidental migration file

--- db/migrate/20180629235303_add_columns_to_measurements.rb
+++
@@ -1,9 +0,0 @@
-# class AddColumnsToMeasurements < ActiveRecord::Migration[5.1]
-#   def change
-#     add_column :measurements, :hem_width, :float
-#     add_column :measurements, :shoulder_to_shoulder, :float
-#     add_column :measurements, :body_length, :float
-#     add_column :measurements, :sleeve_shoulder, :float
-#     add_column :measurements, :sleeve_opening, :float
-#   end
-# end
